523c47834d5f1e1315b3e5ec7146a1693fbdbe05
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 23:04:21 +0400

    feat: Сверстал layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,14 +2,16 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
 import { Navbar } from "../../components/Navbar/Navbar";
 
+import styles from "./MainLayout.module.scss";
+
 export const MainLayout = () => {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <section>
+      <section className={styles.container}>
         <Navbar />
         <Outlet />
       </section>
-    </>
+    </div>
   );
 };

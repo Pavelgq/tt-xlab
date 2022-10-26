46d47813f63933062a464041621ca2238175e9c4
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 13:28:49 +0400

    fix: Поправил роуты

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout/MainLayout";
 import { Address } from "./pages/Address/Address";
 import { News } from "./pages/News/News";
 
 export const RoutesList = () => {
   return (
     <Routes>
-      <Route path="/" element={<News />} />
-      <Route path="/address" element={<Address />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<News />} />
+        <Route path="address" element={<Address />} />
+      </Route>
     </Routes>
   );
 };

9097ad314f962cc293c9ecc070c7cddc18491b82
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 14:16:22 +0400

    feat: Добавил компонент Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,16 @@
+import { ReactComponent as LogoIcon } from "../../assets/svg/logo.svg";
+import { UserBadge } from "../UserBadge/UserBadge";
+
+import styles from "./Header.module.scss";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <LogoIcon />
+        <h1 className={styles.title}>Wrench CRM</h1>
+      </div>
+      <UserBadge />
+    </header>
+  );
 };

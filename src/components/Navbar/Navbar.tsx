54110f3bf28ab35cf2968bd6c7425be320eff154
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 12:42:06 +0400

    refactor: Убрал лишнее

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,6 @@
+import styles from "./Navbar.module.scss";
+import { NavbarProps } from "./Navbar.props";
+
+export const Navbar = ({}: NavbarProps) => {
+  return <>Navbar</>;
+};
---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header/Header";
+import { Navbar } from "../../components/Navbar/Navbar";
+
+export const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <Navbar />
+        <Outlet />
+      </section>
+    </>
+  );
+};

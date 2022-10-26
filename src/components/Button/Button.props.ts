d18f70014115e5e4c5eca4f7492f6ab58a9f31b3
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 13:42:34 +0400

    fix: Поправил пропсы компонента Button

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export interface ButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  className: string
+  className?: string
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import cn from "classnames";
 import styles from "./Button.module.scss";
 import { ButtonProps } from "./Button.props";
 
-export const Button = ({ className, children, ...props }: ButtonProps) => {
+export const Button = ({ className = "", children, ...props }: ButtonProps) => {
   return (
     <button type="button" className={cn(styles.button, className)} {...props}>
       {children}

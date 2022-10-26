fce731a4f0758eba200f5df9e74d595d09dbe632
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-26 23:04:51 +0400

    feat: Сверстал страницы News и Address

--- src/pages/Address/Address.tsx
+++ src/pages/Address/Address.tsx
@@ -1,3 +1,19 @@
+import cn from "classnames";
+import { Paper } from "../../components/Paper/Paper";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import styles from "./Address.module.scss";
+
 export const Address = () => {
-  return <div>Address</div>;
+  return (
+    <section className={styles.container}>
+      <h2 className={"title"}>Поиск адресов</h2>
+      <span className={styles.subTitle}>Введите интересующий вас адрес</span>
+      <SearchBar className={styles.search} />
+
+      <Paper className={styles.result}>
+        <h2 className={styles.title}>Адреса</h2>
+        {/* <SearchResult /> */}
+      </Paper>
+    </section>
+  );
 };
--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,3 +1,18 @@
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import styles from "./News.module.scss";
+import { newsItems } from "./NewsData";
+
 export const News = () => {
-  return <div>News</div>;
+  return (
+    <section className={styles.container}>
+      <h2 className="title">Новости</h2>
+      {newsItems.length > 0 &&
+        newsItems.map((item) => (
+          <div key={item.title} className={styles.news}>
+            <h4 className={styles.subtitle}>{item.title}</h4>
+            <p className={styles.text}>{item.value}</p>
+          </div>
+        ))}
+    </section>
+  );
 };
